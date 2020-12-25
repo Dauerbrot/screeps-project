9ad00f93ea0c53a0b8bab3788773b15e786bc09b
Tobi <toah@gmx.net>
2020-12-25 02:10:54 +0100

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,76 @@
-import {ErrorMapper} from "utils/ErrorMapper";
-import json = Mocha.reporters.json;
+import { ErrorMapper } from "utils/ErrorMapper";
+import { RoleHarvester } from "./roles/role.harvester";
 
 const HARVESTER = "harvester";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
+const creepsInGame = Game.creeps;
+type mapIdentifierType = Record<string, RoleHarvester>;
+const mapOfHarvesterCreeps: mapIdentifierType = {};
+
 function removeMissingCreeps() {
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
+    if (!(name in creepsInGame)) {
       delete Memory.creeps[name];
     }
   }
 }
 
+function generateCreepRoleByCreep(creepsInGameElement: Creep) {
+  const harvesterName = creepsInGameElement.name.toString();
+  console.log(harvesterName);
+  console.log(mapOfHarvesterCreeps[harvesterName] === undefined);
+  console.log(creepsInGameElement.memory.role);
+  console.log(HARVESTER === creepsInGameElement.memory.role);
+  if (HARVESTER === creepsInGameElement.memory.role && mapOfHarvesterCreeps[harvesterName] === undefined) {
+    mapOfHarvesterCreeps[harvesterName] = new RoleHarvester(creepsInGameElement);
+  }
+}
+
+function generateRoleName() {
+  const id = Game.time;
+  const newName = "Harvester" + id.toString(10);
+  console.log("Spawning new harvester: " + newName);
+  return newName;
+}
+
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  const harvesterCounter = _.filter(Game.creeps, creeps => HARVESTER === creeps.memory.role);
+  const harvesterCounter = _.filter(creepsInGame, creeps => HARVESTER === creeps.memory.role);
 
-  if (harvesterCounter.length < 2) {
-    const id = Game.time;
-    const newName = "Harvester" + id.toString(10);
-    console.log("Spawning new harvester: " + newName);
-    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+  for (const spawnsIndex in Game.spawns) {
+    const spawn = Game.spawns[spawnsIndex];
+    const energy = spawn.energy;
+    const energyCapacity = spawn.energyCapacity;
+    if (energyCapacity === energy && harvesterCounter.length < 10) {
+      const harvesterName = generateRoleName();
+      console.log("i spawn with the name" + harvesterName);
+      spawnHarvesterCreep(spawn, harvesterName);
+    }
+  }
+
+  function spawnHarvesterCreep(spawn: StructureSpawn, newName: string) {
+    spawn.spawnCreep([WORK, CARRY, MOVE], newName, {
       memory: {
         role: "harvester"
       }
     });
   }
 
+  if (harvesterCounter.length < 2) {
+    const newName = generateRoleName();
+    const spawn = Game.spawns["Spawn1"];
+    spawnHarvesterCreep(spawn, newName);
+  }
+  // initialize all creeps as objects
+  for (const creepName in creepsInGame) {
+    generateCreepRoleByCreep(creepsInGame[creepName]);
+  }
+
+  for (const key in mapOfHarvesterCreeps) {
+    mapOfHarvesterCreeps[key].runForTick();
+  }
   console.log(`Current game tick is ${Game.time}`);
   removeMissingCreeps();
 });
